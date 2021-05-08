fd33c3ccb68322d4bf44d42d148cf4a93e99bf50
admin <admin@admin.com>
2021-05-08 16:27:11 +0800

    5

--- src/main/java/com/xiao/test1.java
+++ src/main/java/com/xiao/test1.java
@@ -8,6 +8,5 @@ public class test1 {
         System.out.println("pull test");
         System.out.println("gitee test");
         System.out.println("gitee pull test");
-        System.out.println("新GitHub pull test");
     }
 }
