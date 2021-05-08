09d68899a76782985f0bfabb371232e402f5002b
admin <admin@admin.com>
2021-05-08 15:40:01 +0800

    3

--- src/main/java/com/xiao/test1.java
+++ src/main/java/com/xiao/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("push test");
+        System.out.println("push test1");
     }
 }
